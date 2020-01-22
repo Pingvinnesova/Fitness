b674aad6a8dc42d6ec6c5355571c7280b3cf9eff
Polosat <ekaterina.mihnevich@mail.ru>
2020-01-22 22:44:21 +0300

    Добавляет слайдер

--- build/js/main.js
+++ build/js/main.js
@@ -10,3 +10,33 @@ anchor.addEventListener('click', function (e) {
     block: 'start'
   });
 });
+
+// slick
+
+$(document).ready(function(){
+  $('.trainers__list').slick({
+    infinite: false,
+    speed: 350,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    variableWidth: true,
+    nextArrow: document.querySelector('.trainers__switch--next'),
+    prevArrow: document.querySelector('.trainers__switch--prev'),
+    responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+  });
+});
