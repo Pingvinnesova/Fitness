3540b2088f5f180ef63e02f24d5dd43ee234ddd1
Polosat <ekaterina.mihnevich@mail.ru>
2020-02-06 07:29:30 +0300

    Добавляет правки в блок Advantages и добавляет самовызывающуюся функцию

--- source/js/main.js
+++ source/js/main.js
@@ -1,43 +1,5 @@
 'use strict';
 
-// input
-
-var input = document.querySelector('#order__tel');
-
-if (input) {
-  window.addEventListener('DOMContentLoaded', function () {
-    function setCursorPosition(pos, elem) {
-      elem.focus();
-      if (elem.setSelectionRange) {
-        elem.setSelectionRange(pos, pos);
-      } else if (elem.createTextRange) {
-        var range = elem.createTextRange();
-        range.collapse(true);
-        range.moveEnd('character', pos);
-        range.moveStart('character', pos);
-        range.select();
-      }
-    }
-
-    function mask(event) {
-      var matrix = '+7 (___) ___ ____',
-        i = 0,
-        def = matrix.replace(/\D/g, ''),
-        val = this.value.replace(/\D/g, '');
-      if (def.length >= val.length) val = def;
-      this.value = matrix.replace(/./g, function(a) {
-        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
-      });
-      if (event.type === 'blur') {
-        if (this.value.length === 2) this.value = '';
-      } else setCursorPosition(this.value.length, this);
-    }
-    input.addEventListener('input', mask, false);
-    input.addEventListener('focus', mask, false);
-    input.addEventListener('blur', mask, false);
-  });
-}
-
 // якорь
 
 var anchor = document.querySelector('.scroll-to');
@@ -53,13 +15,12 @@ if (anchor) {
   });
 }
 
-var mySwiperTrainers = new Swiper('.trainers__slider', {
+// sliderTrainers
+
+window.swiper(document.querySelector('.trainers__slider'), {
   navigation: {
     nextEl: '.trainers__switch--next',
     prevEl: '.trainers__switch--prev',
-  },
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
   breakpoints: {
     320: {
@@ -80,19 +41,59 @@ var mySwiperTrainers = new Swiper('.trainers__slider', {
   }
 });
 
-// feedback-slider
+// sliderFeedback
 
-var mySwiperFeedback = new Swiper('.feedback-slider', {
+window.swiper(document.querySelector('.feedback__slider'), {
   slidesPerView: 1,
   slidesPerGroup: 1,
   navigation: {
     nextEl: '.feedback__switch--next',
     prevEl: '.feedback__switch--prev',
   },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
+
+// sliderTrainers
+
+// var mySwiperTrainers = new Swiper('.trainers__slider', {
+//   navigation: {
+//     nextEl: '.trainers__switch--next',
+//     prevEl: '.trainers__switch--prev',
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       slidesPerGroup: 1,
+//       spaceBetween: 30,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       slidesPerGroup: 2,
+//       spaceBetween: 30
+//     },
+//     1200: {
+//       slidesPerView: 4,
+//       slidesPerGroup: 4,
+//       spaceBetween: 40,
+//     },
+//   }
+// });
+
+// sliderFeedback
+
+// var mySwiperFeedback = new Swiper('.feedback-slider', {
+//   slidesPerView: 1,
+//   slidesPerGroup: 1,
+//   navigation: {
+//     nextEl: '.feedback__switch--next',
+//     prevEl: '.feedback__switch--prev',
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
 
 // footer
 
@@ -153,4 +154,14 @@ if (membershipItem || membershipButton) {
 
 if (membershipLink) {
   membershipLinkActive.focus();
+}
+
+// input
+
+var input = document.querySelector('#order__tel');
+
+if (input) {
+  document.oninput = function () {
+    input.value = input.value.replace(/\D/g, '');
+  };
 }
